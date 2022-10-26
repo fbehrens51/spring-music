a78d1ecaa055b2f5d4fd38506f91b5ca77081121
fbehrens51 <fbehrens@vmware.com>
2022-10-26 11:15:31 -0400

    added spring security and permitAll for /actuator

---
+++ src/main/java/org/cloudfoundry/samples/music/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.cloudfoundry.samples.music.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(
+            HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
+}
+
